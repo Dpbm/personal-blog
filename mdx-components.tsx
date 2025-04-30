96c227189dfaa7b6daa11f8c0fa4e979e69faa7e
Dpbm <dpbm136@gmail.com>
2025-04-30 14:52:38 -0300

    fixed images

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,17 @@
 import type { MDXComponents } from 'mdx/types';
+import Image, { ImageProps } from 'next/image';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
+		img: (props) => (
+			<Image
+				sizes='100vw'
+				width={500}
+				height={500}
+				style={{ width: '100%', height: 'auto' }}
+				{...(props as ImageProps)}
+			/>
+		),
 		...components,
 	};
 }
