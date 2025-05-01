8b8aa988bf7f4f79d0acc2321a81ecea3ab1deaf
Dpbm <dpbm136@gmail.com>
2025-05-01 14:55:03 -0300

    update datetime format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,6 +15,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
 	return posts.map((post: PostCardData) => ({
 		url: `${prodURL}${buildPostURL(post.slug)}`,
-		lastModified: new Date(post.date).toUTCString(),
+		lastModified: new Date(post.date).toISOString(),
 	}));
 }
