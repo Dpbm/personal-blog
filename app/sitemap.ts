0825a00121f3ff3b80a3f91198caf926072c5902
Dpbm <dpbm136@gmail.com>
2025-05-02 10:39:52 -0300

    updated sitemap date format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,8 +13,16 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	await db.connect();
 	const posts = await db.getAllPosts();
 
-	return posts.map((post: PostCardData) => ({
-		url: `${prodURL}${buildPostURL(post.slug)}`,
-		lastModified: new Date(post.date).toISOString(),
-	}));
+	return posts.map((post: PostCardData) => {
+		const date = new Date(post.date);
+
+		const year = date.getFullYear();
+		const month = String(date.getMonth()).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+
+		return {
+			url: `${prodURL}${buildPostURL(post.slug)}`,
+			lastModified: `${year}-${month}-${day}`,
+		};
+	});
 }
