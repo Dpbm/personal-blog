87bf1de666d303853be7f4965bab3605878f6385
Dpbm <dpbm136@gmail.com>
2025-05-04 09:55:05 -0300

    fixed redirect post

--- app/utils.ts
+++ app/utils.ts
@@ -1,4 +1,4 @@
-export const buildPostURL = (slug: string) => `/posts/${slug}`;
+export const buildPostURL = (slug: string) => `/posts/${slug}/`;
 export const getDateString = (date: Date) =>
 	`${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
 export const prodURL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
